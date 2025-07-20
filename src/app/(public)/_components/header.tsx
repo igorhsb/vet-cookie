88bac64a33313283ab8c471032a15f9bf2276a2a
Igor Henrique <igor.barbosa@levty.com>
2025-07-20 18:32:54 -0300

    header update

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import {
@@ -8,22 +10,53 @@ import {
     SheetTitle,
     SheetTrigger,
 } from '@/components/ui/sheet';
-import { Menu } from 'lucide-react';
+import { LogIn, Menu } from 'lucide-react';
+import React from 'react';
 
 export function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const session = true;
+
+    const navItems = [
+        { href: '#professionals', label: 'Profissionais' },
+        { href: '#services', label: 'Serviços' },
+    ];
+
+    const NavLinks = () => (
+        <>
+            {navItems.map((item) => (
+                <Button
+                    onClick={() => setIsOpen(false)}
+                    key={item.href}
+                    asChild
+                    className="bg-transparent hover:bg-transparent text-black shadow-none"
+                >
+                    <Link href={item.href}>{item.label}</Link>
+                </Button>
+            ))}
+            {session ? (
+                <Link href="/dashboard" className='flex items-center justify-center gap-2 text-black'>Painel da clinica</Link>
+            ) : (
+                <Button>
+                    <LogIn /> Login
+                </Button>
+            )}
+        </>
+    );
+
     return (
         <header className="fixed top-0 right-0 left-0 z-[999] py-4 px-6 bg-white">
             <div className="container mx-auto flex items-center justify-between">
-                
-				<Link href="/" className="text-3xl font-bold text-zinc-900">
+                <Link href="/" className="text-3xl font-bold text-zinc-900">
                     Vet<span className="text-emerald-500">Cookie</span>
                 </Link>
 
                 <nav className="hidden md:flex items-center">
-                    <a href="#"> Profissionais </a>
+                    <NavLinks />
                 </nav>
 
-                <Sheet>
+                <Sheet open={isOpen} onOpenChange={setIsOpen}>
                     <SheetTrigger asChild className="md:hidden">
                         <Button
                             className="text-black hover:bg-transparent"
@@ -35,14 +68,16 @@ export function Header() {
                     </SheetTrigger>
                     <SheetContent
                         side="right"
-                        className="w-[240px] sm:w-[300px] z-[9999]"
+                        className="flex flex-col py-0.5 items-center w-[240px] sm:w-[300px] z-[9999]"
                     >
-                        <SheetTitle>Menu</SheetTitle>
+                        <SheetTitle className="font-bold text-2xl">
+                            Menu
+                        </SheetTitle>
                         <SheetHeader></SheetHeader>
                         <SheetDescription>Veja nossos links</SheetDescription>
 
-                        <nav>
-                            <a href="#"> Profissionais </a>
+                        <nav className="flex flex-col space-y-4 mt-6">
+                            <NavLinks />
                         </nav>
                     </SheetContent>
                 </Sheet>
