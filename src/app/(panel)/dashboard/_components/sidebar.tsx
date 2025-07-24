8360f4050b8a9b562beace6185d2d213982871eb
Igor Henrique <igor.barbosa@levty.com>
2025-07-24 18:43:03 -0300

    Finalizando sidebar 02

--- src/app/(panel)/dashboard/_components/sidebar.tsx
+++ src/app/(panel)/dashboard/_components/sidebar.tsx
@@ -1,50 +1,175 @@
-"use client"
-import { useState } from "react";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
+'use client';
+import { useState } from 'react';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-import { Button } from "@/components/ui/button";
-import { Banknote, CalendarCheck2, Folder, List, Settings } from "lucide-react";
-import Link from 'next/link'
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetTitle,
+    SheetTrigger,
+} from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+import {
+    Banknote,
+    CalendarCheck2,
+    ChevronLeft,
+    ChevronRight,
+    Folder,
+    List,
+    Settings,
+} from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
+import logoImg from '../../../../../public/logo-odonto.png';
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from '@/components/ui/collapsible';
 
-export function SidebarDashboard({
-    children,
-}: {
-    children : React.ReactNode
-}) {
-
+export function SidebarDashboard({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
     const [isCollapsed, setIsCollapsed] = useState(false);
 
     return (
         <div className="flex min-h-screen w-full">
-            <div className={clsx("flex flex-1 flex-col transition-all duration-300", {
-                "md:ml-20" : isCollapsed,
-                "md:ml-64" : !isCollapsed
-            })}>
+            <aside
+                className={clsx(
+                    'flex flex-col border-r bg-background transition-all duration-300p-4 h-full px-4',
+                    {
+                        'w-20': isCollapsed,
+                        'w-64': !isCollapsed,
+                        'hidden md:flex md:fixed': true,
+                    }
+                )}
+            >
+                <div className="mb-6 mt-4">
+                    {!isCollapsed && (
+                        <Image
+                            src={logoImg}
+                            alt="Logo VetCookie"
+                            priority
+                            quality={100}
+                            style={{ width: 'auto', height: 'auto' }}
+                        ></Image>
+                    )}
+                </div>
+                <Button
+                    className="bg-gray-100 hover:bg-gray-400 text-zinc-900 self-end"
+                    onClick={() => setIsCollapsed(!isCollapsed)}
+                >
+                    {!isCollapsed ? (
+                        <ChevronLeft className="w-12 h-12"></ChevronLeft>
+                    ) : (
+                        <ChevronRight className="w-12 h-12"></ChevronRight>
+                    )}
+                </Button>
+                {isCollapsed && (
+                    <nav className="flex flex-col gap-1 overflow-hidden mt-3">
+                        <SidebarLink
+                            href="/dashboard"
+                            label="Agendamentos"
+                            pathname={pathname}
+                            isCollapsed={isCollapsed}
+                            icon={<CalendarCheck2 className="w-6 h-6" />}
+                        />
+                        <SidebarLink
+                            href="/dashboard/services"
+                            label="Serviços"
+                            pathname={pathname}
+                            isCollapsed={isCollapsed}
+                            icon={<Folder className="w-6 h-6" />}
+                        />
+                        <SidebarLink
+                                href="/dashboard/profile"
+                                label="Meu perfil"
+                                pathname={pathname}
+                                isCollapsed={isCollapsed}
+                                icon={<Settings className="w-6 h-6" />}
+                            />
+
+                            <SidebarLink
+                                href="/dashboard/plans"
+                                label="Planos"
+                                pathname={pathname}
+                                isCollapsed={isCollapsed}
+                                icon={<Banknote className="w-6 h-6" />}
+                            />
+                    </nav>
+                )}
+                <Collapsible open={!isCollapsed}>
+                    <CollapsibleContent>
+                        <nav className="flex flex-col gap-1 overflow-hidden">
+                            <span className="text:sm text-gray-400 font-medium mt-1 uppercase">
+                                Painel
+                            </span>
+                            <SidebarLink
+                                href="/dashboard"
+                                label="Agendamentos"
+                                pathname={pathname}
+                                isCollapsed={isCollapsed}
+                                icon={<CalendarCheck2 className="w-6 h-6" />}
+                            />
+                            <SidebarLink
+                                href="/dashboard/services"
+                                label="Serviços"
+                                pathname={pathname}
+                                isCollapsed={isCollapsed}
+                                icon={<Folder className="w-6 h-6" />}
+                            />
+                            <span className="text:sm text-gray-400 font-medium mt-1 uppercase">
+                                Configurações
+                            </span>
+                            <SidebarLink
+                                href="/dashboard/profile"
+                                label="Meu perfil"
+                                pathname={pathname}
+                                isCollapsed={isCollapsed}
+                                icon={<Settings className="w-6 h-6" />}
+                            />
+
+                            <SidebarLink
+                                href="/dashboard/plans"
+                                label="Planos"
+                                pathname={pathname}
+                                isCollapsed={isCollapsed}
+                                icon={<Banknote className="w-6 h-6" />}
+                            />
+                        </nav>
+                    </CollapsibleContent>
+                </Collapsible>
+            </aside>
+            <div
+                className={clsx(
+                    'flex flex-1 flex-col transition-all duration-300',
+                    {
+                        'md:ml-20': isCollapsed,
+                        'md:ml-64': !isCollapsed,
+                    }
+                )}
+            >
                 <header className="md:hidden flex items-center justify-between border-b px-2 md:px-6 h-14 z-10 sticky top-0 bg-white">
                     <Sheet>
                         <div className="flex items-center gap-4">
                             <SheetTrigger>
-                                <Button variant="outline" size="icon" className="md:hidden">
-                                    <List className="w-5 h-5"/>
+                                <Button
+                                    variant="outline"
+                                    size="icon"
+                                    className="md:hidden"
+                                >
+                                    <List className="w-5 h-5" />
                                 </Button>
                             </SheetTrigger>
                             <h1 className="text-base md:text-lg font-semibold">
                                 Menu VetCookie
                             </h1>
                         </div>
-                        <SheetContent side="left" className="sm:max-w-xs text-black px-6 py-3">
-                            <SheetTitle>
-                                VetCookie
-                            </SheetTitle>
+                        <SheetContent
+                            side="left"
+                            className="sm:max-w-xs text-black px-6 py-3"
+                        >
+                            <SheetTitle>VetCookie</SheetTitle>
                             <SheetDescription>
                                 Menu adminsatritivo
                             </SheetDescription>
@@ -54,22 +179,24 @@ export function SidebarDashboard({
                                     label="Agendamentos"
                                     pathname={pathname}
                                     isCollapsed={isCollapsed}
-                                    icon={<CalendarCheck2 className="w-6 h-6"/>}
+                                    icon={
+                                        <CalendarCheck2 className="w-6 h-6" />
+                                    }
                                 />
                                 <SidebarLink
                                     href="/dashboard/services"
                                     label="Serviços"
                                     pathname={pathname}
                                     isCollapsed={isCollapsed}
-                                    icon={<Folder className="w-6 h-6"/>}
+                                    icon={<Folder className="w-6 h-6" />}
                                 />
-                                
+
                                 <SidebarLink
                                     href="/dashboard/profile"
                                     label="Meu perfil"
                                     pathname={pathname}
                                     isCollapsed={isCollapsed}
-                                    icon={<Settings className="w-6 h-6"/>}
+                                    icon={<Settings className="w-6 h-6" />}
                                 />
 
                                 <SidebarLink
@@ -77,40 +204,41 @@ export function SidebarDashboard({
                                     label="Planos"
                                     pathname={pathname}
                                     isCollapsed={isCollapsed}
-                                    icon={<Banknote className="w-6 h-6"/>}
+                                    icon={<Banknote className="w-6 h-6" />}
                                 />
                             </nav>
                         </SheetContent>
                     </Sheet>
                 </header>
-                <main className="flex-1 py-4 px-2 md:p-6">
-                    {children}
-                </main>
+                <main className="flex-1 py-4 px-2 md:p-6">{children}</main>
             </div>
-            
         </div>
     );
 }
 
 interface SidebarLinkProps {
-    href : string;
-    icon : React.ReactNode;
-    label : string;
-    pathname : string;
-    isCollapsed : boolean;
+    href: string;
+    icon: React.ReactNode;
+    label: string;
+    pathname: string;
+    isCollapsed: boolean;
 }
 
-function SidebarLink({href, icon, isCollapsed, label, pathname}) {
-    return(
+function SidebarLink({ href, icon, isCollapsed, label, pathname }) {
+    return (
         <Link href={href}>
-            <div className={clsx("flex items-center gap-2 px-3 py-2 rounded-md transition-colors", {
-                "text-white bg-blue-500" : pathname === href,
-                "text-gray-700 hover:bg-gray-100" : pathname !== href
-            }
-            )}>
+            <div
+                className={clsx(
+                    'flex items-center gap-2 px-3 py-2 rounded-md transition-colors',
+                    {
+                        'text-white bg-blue-500': pathname === href,
+                        'text-gray-700 hover:bg-gray-100': pathname !== href,
+                    }
+                )}
+            >
                 <span className="w-6 h-6">{icon}</span>
                 {!isCollapsed && <span>{label}</span>}
             </div>
         </Link>
-    )
+    );
 }
